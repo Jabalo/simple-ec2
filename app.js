67734962059866a57fccfb698a56c9e1a216bfb3
Jabalo <juliane.yamasaki@weltel.org>
2020-02-24 13:05:26 -0800

    add invoke lambda

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+const aws = require('aws-sdk');
+const lambda = new aws.Lambda();
+
+app.get("/", (req, res) => {
+    res.status(200).send("Test EC2");
+
+    const params = {
+        FunctionName: 'shasta-dev-sms'
+    };
+
+    return lambda.invoke(params, (err, data) => {
+        if (err) console.log('ERROR', err);
+        else console.log('SUCCESS', data);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
